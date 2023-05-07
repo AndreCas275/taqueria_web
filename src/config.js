2d839f7dcea7281c5be08b09e431eb4cc6787d43
AndreCas275 <andrius1001@hotmail.com>
2023-05-07 13:12:32 -0600

    7 commit

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     puertos: {
         PORT: process.env.PORT || 4000,
-        DB_HOST: process.env.DB_HOST || 'localhost',
-        DB_USER: process.env.DB_USER || 'root',
-        DB_PASSWORD: process.env.DB_PASSWORD || 'n0m3l0',
-        DB_NAME: process.env.DB_NAME || 'pedidos',
+        DB_HOST: process.env.DB_HOST || 'b3anpalbidmdb6zmf7ht-mysql.services.clever-cloud.com',
+        DB_USER: process.env.DB_USER || 'un1pc4gmbueuku7o',
+        DB_PASSWORD: process.env.DB_PASSWORD || 'tsUhD5iW4RGHTXbcIUJI',
+        DB_NAME: process.env.DB_NAME || 'b3anpalbidmdb6zmf7ht',
         DB_PORT: process.env.DB_PORT || 3306
     }
 };
