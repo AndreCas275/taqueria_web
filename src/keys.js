52200f74e6cd620b879776a6cc50ad27dfd7236b
AndreCas275 <andrius1001@hotmail.com>
2023-05-07 12:52:19 -0600

    third commit

--- src/keys.js
+++ src/keys.js
@@ -5,7 +5,7 @@ module.exports = {
         host: puertos.DB_HOST,
         user: puertos.DB_USER,
         password: puertos.DB_PASSWORD,
-        port: puertos.DB_PORT,
-        database: puertos.DB_NAME
+        database: puertos.DB_NAME,
+        port: puertos.DB_PORT
     }
 };
