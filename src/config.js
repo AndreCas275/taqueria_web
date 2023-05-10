cab90f36d5a27300262b57d22f7b5668e41867d1
AndreCas275 <andrius1001@hotmail.com>
2023-05-09 20:37:15 -0600

    10 commit

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ module.exports = {
         DB_USER: process.env.DB_USER || 'un1pc4gmbueuku7o',
         DB_PASSWORD: process.env.DB_PASSWORD || 'tsUhD5iW4RGHTXbcIUJI',
         DB_NAME: process.env.DB_NAME || 'b3anpalbidmdb6zmf7ht',
-        DB_PORT: process.env.DB_PORT || 3306
+        DB_PORT: process.env.DB_PORT || 3306 //,
+            //ssl:true
     }
 };
